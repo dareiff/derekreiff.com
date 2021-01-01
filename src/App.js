8696ece26af9967b974f8fe29ca3f03783cf9ac0
Derek Reiff <derekr@omnigroup.com>
2021-01-01 11:29:40 -0800

    First commit of derekreiff.com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>derek reiff.</h1>
+            Hi, my name is Derek. I{' '}
+            <a href='https://myfrienddereks.com'>make pizza</a>, sometimes I{' '}
+            <a href='https://derekr.net'>play around</a>.
+            <p>
+                Contact: <a href='mailto:d@derek.net'>Email</a>
+            </p>
+        </div>
+    );
 }
 
 export default App;
