d464a357ba1914517c836a3d4c102bbab833ea2e
Derek Reiff <d@derekr.net>
2021-01-31 06:45:19 +0000

    Updates to get away from default react app stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <h1>derek reiff.</h1>
-            Hi, my name is Derek. I{' '}
-            <a href='https://myfrienddereks.com'>make pizza</a>, sometimes I{' '}
-            <a href='https://derekr.net'>play around</a>.
-            <p>
-                Contact: <a href='mailto:d@derek.net'>Email</a>
-            </p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>this is me, derek reiff dot com</h1>
+      Hi, my name is Derek. I{" "}
+      <a href="https://myfrienddereks.com">make pizza</a>, sometimes I{" "}
+      <a href="https://derekr.net">play around</a>.
+      <ul>
+        <li>
+          <a href="mailto:d@derek.net">Email me</a>
+        </li>
+        <li>
+          <a href="tel:206-794-0367">Or text DO NOT EVEN THINK ABOUT CALLING</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
