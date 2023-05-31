12011f987444a1b0b6a323687d1f50f5a70d3dd4
Derek Reiff <d@derekr.net>
2023-05-31 12:58:06 -0700

    Switch to nextjs from react/react-scripts

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+function index() {
+    return (
+        <div className="App">
+            <h1>this is me, derek reiff dot com</h1>
+            Hi, my name is Derek. I{" "}
+            <a href="https://myfrienddereks.com">make pizza</a>, sometimes I{" "}
+            <a href="https://derekr.net">play around</a>.
+            <ul>
+                <li>
+                    <a href="mailto:d@derek.net">Email me</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default index;
